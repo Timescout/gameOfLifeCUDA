786006c8809539b304446ffe5c4395f459032ce2
Timescout <timepolicescout@gmail.com>
2026-06-13 00:00:56 -0700

    Implements the DataSet Class.

--- code/source/simulation/DataSet.h
+++ code/source/simulation/DataSet.h
@@ -4,7 +4,7 @@
  * 6/12/26
  * Personal Project
  * 
- * This file defines a class which holds a set of game of life cells, a single "frame" or "step" of the simulation.
+ * This file declares a class which holds a set of game of life cells, a single "frame" or "step" of the simulation.
  */
 
 
@@ -12,6 +12,7 @@
 
 
 #include <vector>
+#include <stdexcept>
 
 
 namespace game_of_life {
@@ -19,13 +20,42 @@ namespace game_of_life {
 
     class DataSet {
     private:
+        ///// Member Variables /////    
+
         std::vector<bool> cells_;
         int number_rows_;
         int number_columns_;
+
+
     public:
-        DataSet();
-        DataSet(DataSet& original);
+        ///// Constructors and Destructors /////
+
+        DataSet(int number_rows = 8, int number_columns = 8);
+
+        DataSet(const DataSet& original);
+
         ~DataSet();
+
+
+        ///// Get and Set functions /////
+
+        int GetNumberRows() const;
+
+        /// @param new_number_rows Must be 1 or greater.
+        void SetNumberRows(int new_number_rows);
+
+        int GetNumberColumns() const;
+
+        /// @param new_number_columns Must be 1 or greater.
+        void SetNumberColumns(int new_number_columns);
+
+        std::vector<bool> GetCells() const;
+
+        bool GetCell(int column, int row) const;
+
+        void SetCell(int column, int row, bool new_state);
+
+        void ToggleCell(int column, int row);
     };
 
 
---
+++ code/source/simulation/Dataset.cu
@@ -0,0 +1,84 @@
+/** DataSet.cu
+ * Game of Life CUDA
+ * Author: Andrew Bergman
+ * 6/12/26
+ * Personal Project
+ * 
+ * This file implements the DataSet class.
+ */
+
+
+#pragma once
+
+
+#include <vector>
+#include <stdexcept>
+
+
+#include "DataSet.h"
+
+
+namespace game_of_life
+{
+
+
+    ///// Constructors and Destructors /////
+
+    DataSet::DataSet(int number_rows, int number_columns) {
+        SetNumberColumns(number_columns);
+        SetNumberRows(number_rows);
+        cells_ = std::vector<bool>(false, GetNumberColumns()*GetNumberRows());
+    }
+
+    DataSet::DataSet(const DataSet& original) {
+        SetNumberColumns(original.GetNumberColumns());
+        SetNumberRows(original.GetNumberRows());
+        cells_ = std::vector<bool>(original.GetCells());
+    }
+
+    DataSet::~DataSet() {}
+
+
+    ///// Get and Set functions /////
+
+    int DataSet::GetNumberRows() const {
+        return number_rows_;
+    }
+
+    void DataSet::SetNumberRows(int new_number_rows) {
+        if (new_number_rows < 1) {
+            throw std::logic_error("Number of rows must be one or greater.");
+        }
+        number_rows_ = new_number_rows;
+    }
+
+    int DataSet::GetNumberColumns() const {
+        return number_columns_;
+    }
+
+    void DataSet::SetNumberColumns(int new_number_columns) {
+        if (new_number_columns < 1) {
+            throw std::logic_error("Number of columns must be one or greater.");
+        }
+        number_columns_ = new_number_columns;
+    }
+
+    std::vector<bool> DataSet::GetCells() const {
+        return cells_;
+    }
+
+    bool DataSet::GetCell(int column, int row) const {
+        return cells_.at(row*GetNumberColumns()+column);
+    }
+
+    void DataSet::SetCell(int column, int row, bool new_state) {
+        cells_[row*GetNumberColumns()+column] = new_state;
+    }
+
+    void DataSet::ToggleCell(int column, int row) {
+        SetCell(column, row, !GetCell(column, row));
+    }
+
+
+} // namespace game_of_life
+ 
