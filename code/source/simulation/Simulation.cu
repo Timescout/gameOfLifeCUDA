07f212331977e375251aade0939473171c37c612
Timescout <timepolicescout@gmail.com>
2026-06-14 15:51:55 -0700

    Builds out most of Simulation class.

--- code/source/simulation/Simulation.cu
+++ code/source/simulation/Simulation.cu
@@ -12,12 +12,47 @@
 
 
 #include "Simulation.h"
+#include "DataSet.h"
 
 
-game_of_life::Simulation::Simulation(/* args */)
-{
-}
+namespace game_of_life {
 
-game_of_life::Simulation::~Simulation()
-{
-}
+
+    ///// Constructors and Destructors /////
+
+    Simulation::Simulation(DataSet cell_data) :
+        number_steps_(0) 
+    {
+        SetDataSet(cell_data);
+    }
+
+    Simulation::~Simulation() {}
+
+
+    ///// Get and Set Functions /////
+
+    DataSet Simulation::GetDataSet() const {
+        return cell_data_;
+    }
+
+    void Simulation::SetDataSet(DataSet new_dataset) {
+        cell_data_ = new_dataset;
+    }
+
+    unsigned int Simulation::GetNumberSteps() const {
+        return number_steps_;
+    }
+
+
+    ///// Simulation functions /////
+
+    void Simulation::Simulate(unsigned int number_steps) {
+        // TODO: Implement this
+    }
+
+    void Simulation::SimulateToStep(unsigned int step_number) {
+        // TODO: Implement this
+    }
+
+
+} // namespace game_of_life
--- code/source/simulation/Simulation.h
+++ code/source/simulation/Simulation.h
@@ -17,12 +17,45 @@
 namespace game_of_life {
 
 
-    class Simulation
-    {
+    /// @brief Runs the Game of Life simulation on a Dataset of cells.
+    class Simulation {
     private:
+        ///// Member Variables /////
+
+        /// @brief Contains current cell data.
+        DataSet cell_data_;
+
+        /// @brief Counts the number of steps since the start of the simulation.
+        unsigned int number_steps_;
+
     public:
-        Simulation();
+        ///// Constructors and Destructors /////
+
+        Simulation(DataSet cell_data = DataSet());
+
         ~Simulation();
+
+
+        ///// Get and Set /////
+
+        DataSet GetDataSet() const;
+
+        void SetDataSet(DataSet new_dataset);
+
+        unsigned int GetNumberSteps() const;
+
+        // Note number_steps_ cannot be set with a setter function, it can only be altered via progressing the simulation.
+
+
+        ///// Simulation functions /////
+
+        /// @brief Run the simulation for a number of steps.
+        /// @param number_steps 
+        void Simulate(unsigned int number_steps = 1);
+
+        /// @brief Runs the simulation until the given step number. I.e. If the current number of steps is 10, and 100 is passed to this function the simulation will run for 90 steps. If the step number has already been reached then the function will pass and do nothing.
+        /// @param step_number 
+        void SimulateToStep(unsigned int step_number);
     };
 
 
